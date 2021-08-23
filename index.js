e849eb6dd1a23fb81a60e34e34cd60f8703b4d0e
Domagoj Korman <domagojkorman93@gmail.com>
2021-08-23 10:02:08 +0200

    Create ember cli addon to generate SVGs

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
 'use strict';
 
+const fs = require('fs')
+
 module.exports = {
-  name: require('./package').name
+  name: require('./package').name,
+
+  _options() {
+    return this.app.options.svgComponent;
+  },
+
+  preBuild() {
+    this._options().paths.forEach((path) => {
+      const files = fs.readdirSync(path.input);
+      files.forEach((file) => {
+        const outputDir = `${path.output}/${file.split('.')[0]}`;
+        const inputFile = `${path.input}/${file}`;
+        const outputFile = `${outputDir}/template.hbs`;
+        const fsOptions = {encoding: 'utf-8'};
+        fs.mkdirSync(outputDir, {recursive: true});
+
+        const prevContent = fs.existsSync(outputFile) ? fs.readFileSync(outputFile, fsOptions) : null;
+        const content = fs.readFileSync(inputFile, fsOptions);
+
+        if (prevContent !== content) {
+          fs.writeFileSync(``, content, {encoding: 'utf-8'});
+        }
+      });
+    });
+  },
 };
